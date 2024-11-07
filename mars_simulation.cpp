a496dab0c2db8a5807424a07f296b4673e33ba08
courageousstrawberry <elech2a@gmail.com>
2024-11-06 19:33:23 -0800

    continue creating outline

--- mars_simulation.cpp
+++ mars_simulation.cpp
@@ -6,3 +6,22 @@
 // Define main function
     // Initialize a map to store Mars climate data for each year
         // Each key contains an array of lists for surface temperature, sun distance and rock abundance
+
+// Open an external file to read initial data about previous climate conditions on Mars
+        // If file does not open, print an error and exit
+
+    // Read data from file and populate map
+        // For each line, extract surface temperature, sun distance, and rock abundance data
+
+    // Close the file
+
+    // Begin a time-based simulation for environmental changes
+        // Randomly pick an index for a year on the map, these will be the starting conditions
+        // For 30 time intervals (each time interval represents 3 earth-years)
+            // Randomly pick an index from 1-3,
+                // 1 represents a dust storm
+                // 2 represents a volccanic erruption
+                // 3 represents measuring data
+            // Depending on the index, simulate current changes on the planet
+                // If it's a dust storm, reduce the temperature and rock abundance value (erosion of rocks)
+                // 
